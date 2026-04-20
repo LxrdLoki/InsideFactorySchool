625d3cd89f39c80d13a8b4e41440c737add77425
LxrdLoki <melvinvd69@outlook.com>
2026-04-20 14:32:58 +0200

    added components for login and register, and added the routes to routing.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { Dashboard } from './dashboard/dashboard';
 import { Layout } from './layout/layout';
+import { Login } from './login/login';
+import { Register } from './register/register';
 
 export const routes: Routes = [
   {
@@ -10,6 +12,14 @@ export const routes: Routes = [
       {
         path: '',
         component: Dashboard
+      },
+      {
+        path: 'login',
+        component: Login
+      },
+      {
+        path: 'register',
+        component: Register
       }
     ]
   }
